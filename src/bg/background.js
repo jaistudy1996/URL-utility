d5b858f6742b7f0d87d773084c72fec1abef2924
Jayant Arora <jaistudy1996@gmail.com>
2017-09-03 02:07:21 -0400

    test feture and set widht and height of image displayed

--- src/bg/background.js
+++ src/bg/background.js
@@ -9,16 +9,44 @@ chrome.runtime.onConnect.addListener(function(port){
   });
 });
 
+chrome.webRequest.onHeadersReceived.addListener(
+    function(info) {
+      console.log(info);
+      var headers = info.responseHeaders;
+      var index = headers.findIndex(x=>x.name.toLowerCase() == "x-frame-options");
+      if (index !=-1) {
+        headers.splice(index, 1);
+      }
+      return {responseHeaders: headers};
+    },
+    {
+        urls: ['<all_urls>'], //
+        types: ['sub_frame']
+    },
+    ['blocking', 'responseHeaders']
+);
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
   console.log(sender.tab ?
                 "from a content script:" + sender.tab.url :
                 "from the extension");
   let xhr = new XMLHttpRequest();
-  xhr.responseType = 'json';
+  xhr.responseType = 'text';
   xhr.onreadystatechange = function(){
     console.log(this);
     if(this.readyState == 4 && this.status == 200){
-        sendResponse({responseURL: xhr.responseURL});
+        let xhrImage = new XMLHttpRequest();
+        let imageURL;
+        imageURL = xhr.responseURL.search("https://") != -1 ? xhr.responseURL.replace("https://", "") : xhr.responseURL.replace("http://", "");
+        xhrImage.responseType = 'text';
+        xhrImage.onreadystatechange = function(){
+          if(this.readyState == 4 && this.status == 200){
+            console.log(this);
+            sendResponse({responseURL: xhr.responseURL, responseWebpage: xhrImage.responseURL});
+          }
+        };
+        xhrImage.open('GET', 'http://www.jayantarora.com/api/screenshot/'+imageURL, true);
+        xhrImage.send();
         console.log(xhr.responseURL);
     }
   };
